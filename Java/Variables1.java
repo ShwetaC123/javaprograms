b16c71e1ece129327cee4a7deb93f2bed73c1a62
ShwetaC123 <143320566+ShwetaC123@users.noreply.github.com>
2025-06-13 04:31:06 +0000

    Java1

---
+++ Java/Variables1.java
@@ -0,0 +1,9 @@
+package Java;
+
+public class Variables1 {
+    public static void main(String[] args) {
+        int age = 10;
+        age = age + 20;
+        System.out.println("Age is: " + age);
+    }
+}
